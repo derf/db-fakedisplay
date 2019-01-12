70d838bb0825cf5a7fa8dcc2eb4e712001da2c1d
Daniel Friesel <derf@finalrewind.org>
2019-01-12 13:56:23 +0100

    support linking to individual trains

--- public/static/collapse.js
+++ public/static/collapse.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
+	if (document.location.hash.length > 1) {
+		var wanted = document.location.hash.replace('#', '');
+		$('div.app > ul > li > .moreinfo, div.infoscreen > ul > li > .moreinfo').each(function() {
+			if ($(this).data('train') == wanted) {
+				$(this).removeClass('collapsed-moreinfo');
+				$(this).addClass('expanded-moreinfo');
+			}
+		});
+	}
 	$('div.app > ul > li, div.infoscreen > ul > li').each(function() {
 		$(this).click(function() {
 			$(this).children('.moreinfo').each(function() {
 				if ($(this).hasClass('expanded-moreinfo')) {
 					$(this).removeClass('expanded-moreinfo');
 					$(this).addClass('collapsed-moreinfo');
+					document.location.hash = '';
 				}
 				else {
 					$('.moreinfo').each(function() {
@@ -15,6 +25,7 @@ $(document).ready(function() {
 					});
 					$(this).removeClass('collapsed-moreinfo');
 					$(this).addClass('expanded-moreinfo');
+					document.location.hash = $(this).data('train');
 				}
 			});
 		});
