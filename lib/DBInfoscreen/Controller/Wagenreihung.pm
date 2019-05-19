596f6deb1adef4200ddb3df4f02e6e98525a33a6
Daniel Friesel <derf@finalrewind.org>
2019-05-19 11:26:22 +0200

    show wagon order even if exact positions on platform are unknown

--- lib/DBInfoscreen/Controller/Wagenreihung.pm
+++ lib/DBInfoscreen/Controller/Wagenreihung.pm
@@ -16,6 +16,17 @@ sub wagenreihung {
 		train_number => $train,
 	);
 
+	if ( $wr->has_bad_wagons ) {
+
+		# create fake positions as the correct ones are not available
+		my $pos = 0;
+		for my $wagon ( $wr->wagons ) {
+			$wagon->{position}{start_percent} = $pos;
+			$wagon->{position}{end_percent}   = $pos + 4;
+			$pos += 4;
+		}
+	}
+
 	$self->render(
 		'wagenreihung',
 		wr        => $wr,
