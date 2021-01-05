f7994434cbc72c9df0bf935f2f57e906909de208
Daniel Friesel <derf@finalrewind.org>
2021-01-05 22:28:07 +0100

    Handle invalid HAFAS XML containing "Wagen \d+ & \d+"

--- lib/DBInfoscreen/Helper/HAFAS.pm
+++ lib/DBInfoscreen/Helper/HAFAS.pm
@@ -113,6 +113,7 @@ sub get_xml_p {
 
 			# More fixes for invalid XML
 			$body =~ s{P&R}{P&amp;R};
+			$body =~ s{Wagen \d+ \K&(?= )}{&amp;};
 
 			# <Attribute [...] text="[...] "[...]"" /> is invalid XML.
 			# Work around it.
