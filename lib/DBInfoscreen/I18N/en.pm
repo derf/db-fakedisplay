8691069a2bc58e17403e0ad9f7c963aff3706525
Birte Kristina Friesel <derf@finalrewind.org>
2023-10-29 13:38:36 +0100

    Add I18N dictionary. derp.

---
+++ lib/DBInfoscreen/I18N/en.pm
@@ -0,0 +1,40 @@
+package DBInfoscreen::I18N::en;
+
+# Copyright (C) 2023 Birte Kristina Friesel
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+use Mojo::Base 'DBInfoscreen::I18N';
+
+our %Lexicon = (
+# common
+	'Stationen in der Umgebung suchen' => 'Find stops nearby',
+# layouts/app
+	'Mehrdeutige Eingabe' => 'Ambiguous input',
+	'Bitte eine Station aus der Liste auswählen' => 'Please select a station from the list',
+	'Zug / Station' => 'Enter train number or station name',
+	'Zug, Stationsname oder Ril100-Kürzel' => 'train, station name, or DS100 code',
+	'Abfahrstafel' => 'Show departures',
+	'Weitere Einstellungen'=> 'Preferences',
+	'Zeiten inkl. Verspätung angeben' => 'Include delay in timestamps',
+	'Verspätungen erst ab 5 Minuten anzeigen' => 'Hide delays below 5 minutes',
+	'Mehr Details' => 'Verbose mode',
+	'Betriebliche Bahnhofstrennungen berücksichtigen (z.B. "Hbf (Fern+Regio)" vs. "Hbf (S)")' => 'Respect split stations; do not join them',
+	'Bereits abgefahrene Züge anzeigen' => 'Include past trains',
+	'Formular verstecken' => 'Hide form',
+	'Nur Züge über' => 'Only show trains via',
+	'Bahnhof 1, Bhf2, ... (oder regulärer Ausdruck)' => 'Station 1, 2, ... (or regular expression)',
+	'Gleise' => 'Platforms',
+	'Ankunfts- oder Abfahrtszeit anzeigen?' => 'Show arrival or departure?',
+	'Abfahrt bevorzugen' => 'prefer departure',
+	'Nur Abfahrt' => 'departure only',
+	'Nur Ankunft' => 'arrival only',
+	'Anzeigen' => 'Submit',
+	'Über DBF' => 'About',
+	'Datenschutz' => 'Privacy',
+	'Impressum' => 'Imprint',
+# landing page
+	'Oder hier angeben:' => 'Or enter manually:',
+);
+
+1;
